38e2d556f08d620c84877f8eb4771337ea569758
vorxl <vorxlaif@gmail.com>
2024-06-28 22:23:32 +0300

    commit 28062024

--- src/hooks/useAdmin.ts
+++ src/hooks/useAdmin.ts
@@ -2,8 +2,5 @@ import {useAppSelector} from "../store/hooks"
 
 export const useAdmin = (): boolean => {
     const role = useAppSelector((state) => state.user.user?.role);
-    if (role === 'admin')
-        return true;
-
-    return false;
+    return role === 'admin';
 }
